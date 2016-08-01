81085f2bf7aa53e8373f5ff7f5967c8189f555eb
Herman Liang <hermanliang80@gmail.com>
2016-08-01 15:27:03 +0800

    update Test.swift

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -6,19 +6,19 @@ import HLSignalView
 
 class TableOfContentsSpec: QuickSpec {
     override func spec() {
-        describe("these will fail") {
+        describe("these will not fail") {
 
-            it("can do maths") {
-                expect(1) == 2
-            }
-
-            it("can read") {
-                expect("number") == "string"
-            }
-
-            it("will eventually fail") {
-                expect("time").toEventually( equal("done") )
-            }
+//            it("can do maths") {
+//                expect(1) == 2
+//            }
+//
+//            it("can read") {
+//                expect("number") == "string"
+//            }
+//
+//            it("will eventually fail") {
+//                expect("time").toEventually( equal("done") )
+//            }
             
             context("these will pass") {
 
